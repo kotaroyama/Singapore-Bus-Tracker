765c2f3d65addfca61ed5942d73c64ccc478b4e6
Kotaro Yamamoto <kotaro.h.yama@gmail.com>
2026-03-05 10:15:28 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
+import os
+
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+def fetch_arrival_info(station_id: str):
+    url = f"https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival"
+    api_key = os.getenv("API_KEY")
+    headers = {"AccountKey": api_key}
+    params = {"BusStopCode": station_id}
+    r = requests.get(
+        url, 
+        headers=headers,
+        params=params,
+    )
+    r.raise_for_status()
+    data = r.json()
+    services = data["Services"]
+    # pprint.pprint(get_next_3_arrivals(services))
+    first_3_arrivals = get_next_3_arrivals(services)
+    print_next_3_arrivals(station_id, first_3_arrivals)
+
+def get_next_3_arrivals(services):
+    first_3_arrivals = []
+    for service in services:
+        # If the first_3_arrivals list dosn't have any elements yet, initialize 
+        # with the first three busses for the first service
+        if not first_3_arrivals:
+            first_3_arrivals.append((service["ServiceNo"], service["NextBus"]))
+            first_3_arrivals.append((service["ServiceNo"], service["NextBus2"]))
+            first_3_arrivals.append((service["ServiceNo"], service["NextBus3"]))
+        else:
+            next_3_busses = [service["NextBus"], service["NextBus2"], service["NextBus3"]]
+            for bus in next_3_busses:
+                # Skip if the estimated arrival time for the bus is empty.
+                if not bus["EstimatedArrival"]:
+                    break
+                arrival_time = datetime.fromisoformat(bus["EstimatedArrival"])
+                for index, arrival in enumerate(first_3_arrivals):
+                    # Check if the item in first_3_arrivals is empty
+                    if arrival[1]["EstimatedArrival"]:
+                        first_arrival_time = datetime.fromisoformat(
+                            arrival[1]["EstimatedArrival"]
+                        )
+                        if arrival_time < first_arrival_time:
+                            first_3_arrivals.insert(
+                                index,
+                                (service["ServiceNo"], bus)
+                            )
+                            first_3_arrivals.pop()
+                            break
+                    else:
+                        first_3_arrivals[index] = (service["ServiceNo"], bus)
+                        break
+        
+    return first_3_arrivals
+
+def get_bus_stops():
+    api_key = os.getenv("API_KEY")
+    headers = {"AccountKey": api_key}
+    bus_stops = []
+    n = 0
+    while True:
+        url = f"https://datamall2.mytransport.sg/ltaodataservice/BusStops?$skip={n}"
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+        data = r.json()
+        if not data["value"]:
+            break
+        bus_stops.extend(data["value"])
+        n += 500
+    return bus_stops
+
+
+def print_next_3_arrivals(station_id, first_3_arrivals):
+    bus_stops = get_bus_stops()
+    for bus_stop in bus_stops:
+        if station_id == bus_stop["BusStopCode"]:
+            current_time = datetime.now(ZoneInfo("Asia/Singapore"))
+            timediff1 = datetime.fromisoformat(first_3_arrivals[0][1]["EstimatedArrival"]) - current_time
+            timediff2 = datetime.fromisoformat(first_3_arrivals[1][1]["EstimatedArrival"]) - current_time
+            timediff3 = datetime.fromisoformat(first_3_arrivals[2][1]["EstimatedArrival"]) - current_time
+            est_arrival1 = current_time + timediff1
+            est_arrival2 = current_time + timediff2
+            est_arrival3 = current_time + timediff3
+            print(f"Stop: {bus_stop['Description']}")
+            print(f"1) {first_3_arrivals[0][0]} {int(timediff1.total_seconds() // 60)} min ({est_arrival1.strftime("%H:%M")})")
+            print(f"2) {first_3_arrivals[1][0]} {int(timediff2.total_seconds() // 60)} min ({est_arrival2.strftime("%H:%M")})")
+            print(f"3) {first_3_arrivals[2][0]} {int(timediff3.total_seconds() // 60)} min ({est_arrival3.strftime("%H:%M")})")
+
+def main():
+    fetch_arrival_info("46091")
+    # bus_stops = get_bus_stops()
+    # print(len(bus_stops))
+
+if __name__ == "__main__":
+    main()
